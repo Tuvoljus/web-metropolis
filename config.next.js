20220a191316b1abfe0064a981e4ac5aecba63f3
Alexander Keil <alexander.keil1@web.de>
2021-03-16 10:56:56 +0100

    test 3

--- config.next.js
+++ config.next.js
@@ -1,8 +1,8 @@
-module.exports = {
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-};
+// module.exports = {
+//   images: {
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//   },
+// };
   
 module.exports = withSass({
   cssModules: true,
@@ -14,7 +14,7 @@ module.exports = withSass({
     if (!config.plugins) config.plugins = [];
     config.plugins.push(
       new TypedCssModulesPlugin({
-        globPattern: 'components/**/*.scss',
+        globPattern: 'components/*.scss',
       }),
     )
     return config;
