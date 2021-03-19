ffae95276d2580a4af34daf46a6b1571a4e9b10c
Alexander Keil <alexander.keil1@web.de>
2021-03-19 15:44:05 +0100

    responsive 2

--- components/Menue.js
+++ components/Menue.js
@@ -4,7 +4,8 @@ import Image from 'next/image'
 
 const Menue = () => {
 const [openedCollapse, setOpenedCollapse] = useState('');
-
+const [customToggler, setCustomToggler] = useState('');
+const [customBorder, setCustomBorder] = useState('');
     
     const showBlog = () => {
         if (openedCollapse != 'show') {
@@ -17,33 +18,48 @@ const [openedCollapse, setOpenedCollapse] = useState('');
 
 const [scrolling, setScrolling] = useState(null);
   const [scrollTop, setScrollTop] = useState(80);
-    const [brandColor, setBrandColor] = useState(null);
-    
-    const color = brandColor;
+    const [brandColor, setBrandColor] = useState(null);    
 
- const divStyle = {
-         color: color,
-         
-  };    
+// Border Color Hamburger Icon    
+const bColor = customBorder;
+const divStyleBorderColor = {
+  borderColor: bColor,
+  //color: bColor         
+};
+
+const color = brandColor;
+const divStyle = {
+  color: color,         
+};
+ 
 
     useEffect(() => {
     function onScroll() {
       let currentPosition = window.pageYOffset; // or use document.documentElement.scrollTop;
       if (currentPosition > 80) {
         // downscroll code
+          setCustomBorder('red');
+          setCustomToggler('custom-toggler');
           setScrolling('c-bg');
           setBrandColor('#ff3333');
       } else {
         // upscroll code
+          setCustomBorder('');
+          setCustomToggler('');
           setScrolling('');
           setBrandColor('');
       }     
         };
 
+      const ChangeHamburgerIcon = () => {
+      divStyleBorderColor = {
+        bColor : {customBorder},
+      };
+    };   
+
      const ChangeBrandColor = () => {
          divStyle = {
-         color: {brandColor},
-         
+         color: {brandColor},         
   }; 
     }
 
@@ -60,7 +76,7 @@ const [scrolling, setScrolling] = useState(null);
               <a className="navbar-brand" href="#page-top" style={divStyle}>
                 Web.Metropolis
               </a>
-              <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <button className={`navbar-toggler navbar-toggler-right ${customToggler}`} style={divStyleBorderColor} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" onClick={showBlog}></span>
               </button>    
                 <div className={`collapse navbar-collapse justify-content-end ${openedCollapse}`} id="navbarResponsive" >
